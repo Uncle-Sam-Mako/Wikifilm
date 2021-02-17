ac0da3885960c45f6edb9436e9c46352bc651daf
uncle_sam <bwsamuel6@gmail.com>
2021-02-17 19:07:56 +0200

    Add modal/

---
+++ modal/dist/js/medea.js
@@ -0,0 +1,56 @@
+//MedeaJS
+let currentModal = null,
+modalFocusElement = [],
+previousFocused = null;
+focusElement = "a, button, input, textarea";
+
+const openModal = function(e){
+    e.preventDefault();
+    //le modal a ouvrir est celui ayant l'id correspondant au href du lien cliqué. Pour cela l'attribut href doit être precédé de '#'
+    currentModal = $(e.target.getAttribute('data-modal'));
+    currentModal.css('display', 'flex');
+    $('body').css('overflow', 'hidden');
+    currentModal.attr('aria-hidden', false);
+    currentModal.attr('aria-modal', true);
+    currentModal.click(closeModal);
+    $('.js-modal-close').click(closeModal);
+    $('.modal-wrapper').click(stopPropagation);
+    modalFocusElement = Array.from(currentModal.find(focusElement));
+    previousFocused = $(':focus');
+    modalFocusElement[0].focus();
+}
+
+const closeModal = function(e){
+    e.preventDefault();
+    currentModal.css('display', 'none');
+    $('body').css('overflow', 'visible');
+    currentModal.attr('aria-hidden', true);
+    currentModal.attr('aria-modal', false);
+    currentModal.off('click', closeModal);
+    $('.js-modal-close').off('click', closeModal);
+    $('.modal-wrapper').off('click', stopPropagation);
+    currentModal = null;
+    previousFocused.focus();
+}
+
+const focusInModal = function(e){
+    e.preventDefault();
+}
+
+const stopPropagation = function(e){
+    e.stopPropagation();
+}
+$('.js-modal-open').each(function(){
+    $(this).on('click', openModal)
+});
+
+$(window).keydown(function(e){
+    switch(e.which){
+        case 27 :
+            closeModal(e);
+        break;
+        case  9:
+            if(currentModal !== null) focusInModal(e);
+        break;
+    }
+})
