610810f3c570f30c0385c5e472fbc6dd2ea81036
uncle_sam <bwsamuel6@gmail.com>
2021-02-19 17:29:24 +0200

    Update app.js & search.html : Get newest movies from API

--- dist/js/app.js
+++ dist/js/app.js
@@ -18,6 +18,7 @@ const inputSearch = document.querySelector("#inputSearch");
 const buttonSearch = document.querySelector("#buttonSearch");
 const movieSearched = document.querySelector(".result-section .movie-container");
 
+
 function search(page){
     const value = inputSearch.value;
     newUrl = url + "&query=" + value;
@@ -26,7 +27,7 @@ function search(page){
         .then((res) => res.json())
         .then((data) => {
             const movies = data.results; 
-            setMoviesSearched(movies);
+            setMoviesSearched(movies, moviesBlock);
             setResultTitle(data, value);
             console.log(movies);
         })
@@ -34,10 +35,35 @@ function search(page){
             console.log("Error", error);
         })
 }
-
+function getNewestMovies(){
+    const url = "https://api.themoviedb.org/3/movie/upcoming?api_key=834aaad6249c99581606d4c68f11385b&language=en-US&page=1";
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            const movies = data.results; 
+            setNewestMovies(movies);
+            console.log(movies);
+        })
+        .catch(() =>{
+            console.log("Error", error);
+        })
+}
 function setResultTitle(resultFound, value){
     const resultTitle = document.querySelector('.result-section h2');
     resultTitle.innerHTML = `Results for "${value}"`;
+}
+
+function setNewestMovies(items){
+    const moviesBlock = document.querySelector('.newest-section .movie-container .flex');
+    for(let i = 0, l = items.length; i<l; i++ ){
+        let item = items[i];
+        let movieItemTemplate = document.getElementById("movie-template");
+        let movieItem = document.importNode(movieItemTemplate.content, true);
+        movieItem.querySelector("a").dataset.movie_id = item.id;
+        movieItem.querySelector(".movie-poster img").src = item.poster_path ? IMAGE_PATH + item.poster_path : 'https://via.placeholder.com/300x450?text=No+poster';
+        movieItem.querySelector('.movie-title p').innerHTML=item.title;
+        moviesBlock.appendChild(movieItem);
+    }
 }
 
 function setMoviesSearched(items){
@@ -45,7 +71,6 @@ function setMoviesSearched(items){
     const moviesBlock = document.querySelector('.result-section .movie-container .flex');
     for(let i = 0, l = items.length; i<l; i++ ){
         let item = items[i];
-        let numberOf = 0;
         if(item.media_type==="tv" || item.media_type==="movie"){
             let movieItemTemplate = document.getElementById("movie-template");
             let movieItem = document.importNode(movieItemTemplate.content, true);
@@ -56,7 +81,6 @@ function setMoviesSearched(items){
             }else{
                 movieItem.querySelector('.movie-title p').innerHTML=item.name;
             }
-            numberOf++;
             moviesBlock.appendChild(movieItem);
         }
     }
@@ -78,4 +102,6 @@ inputSearch.addEventListener('keyup', function(e){
         search();
     }
 })
+
 }
+getNewestMovies();
